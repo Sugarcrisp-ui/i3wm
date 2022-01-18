169ee571d7f36a487260a99f260bfe89dbc786d0
Brett Crisp <brettcrisp2@gmail.com>
2022-01-18 11:30:56 +0700

    organizing app list

--- Personal/110-install-core-software-v3.sh
+++ Personal/110-install-core-software-v3.sh
@@ -45,59 +45,46 @@ func_category() {
 func_category Core Software
 
 list=(
-aic94xx-firmware
-atom
-bitwarden
-calibre
-catfish
-caprine
-celluloid
+# Internet
 chromium
+firefox
+google-chrome
+qbittorrent
+speedtest-cli-git
+qtwebflix-git
+
+# Multimedia
+ffmpeg
+celluloid
+losslesscut-bin
+openshot
+qbittorrent
+simplescreenrecorder
+spotify
+vlc
+
+# Utilities
+bitwarden
+catfish
 clipgrab
-#clipman
 copyq
 cronie
-discord
 etcher-bin
-firefox
-flatpak
 font-manager-git
-geany
-gedit
 gnome-calculator
-google-chrome
 gpick
 grsync
-grub-btrfs
 grub-customizer
+gthumb
 insync
-libreoffice-still
-losslesscut-bin
 meld
-micro
-openshot
-pamac
-paprefs
-paru-bin
-polybar
-powerline
-qbittorrent
-qtwebflix-git
 ranger
 seahorse
-simplescreenrecorder
-speedtest-cli-git
-spotify
-sublime-text-4
-#syncthing
+sshfs
 timeshift
 timeshift-autosnap
-ttf-font-awesome
 unzip
-upd72020x-fw
-virtualbox
-wd719x-firmware
-whatsapp-nativefier
+wget
 xfce4-appfinder
 xfce4-notifyd
 xfce4-power-manager
@@ -105,9 +92,43 @@ xfce4-screenshooter
 xfce4-settings
 xfce4-taskmanager
 xfce4-terminal
+zip
+
+# Documents and Text
+atom
+calibre
+geany
+gedit
+libreoffice-still
+micro
+sublime-text-4
 xournalpp
 xreader
-zip
+
+# Communication
+caprine
+discord
+signal-desktop
+whatsapp-nativefier
+
+# Others
+aic94xx-firmware
+flatpak
+grub-btrfs
+pamac
+paprefs
+paru-bin
+polybar
+powerline
+ttf-font-awesome
+upd72020x-fw
+wd719x-firmware
+
+# Custom
+
+# Developer
+virtualbox
+
 )
 
 count=0
