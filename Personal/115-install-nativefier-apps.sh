4e36efd8cf397482bd36ab5c29fba605d080e848
Brett Crisp <brettcrisp2@gmail.com>
2023-05-06 20:47:56 +0700

    removed netflix from nativefier install

--- Personal/115-install-nativefier-apps.sh
+++ Personal/115-install-nativefier-apps.sh
@@ -8,13 +8,11 @@ set -e
 ###############################################################################
 
 
-nativefier --name "Facebook Messenger" "https://www.facebook.com/messages/t/6143117129045315/" --icon /home/brett/Pictures/messenger.png
+nativefier --name "Facebook Messenger" "https://www.facebook.com/messages/t/6143117129045315/"
 
-nativefier --name "Twitter" "https://www.twitter.com" --icon /home/brett/Pictures/twitter-icon.png
+nativefier --name "Twitter" "https://www.twitter.com"
 
-nativefier --name "Whatsapp" "https://web.whatsapp.com" --icon /home/brett/Pictures/whatsapp-icon.png
-
-nativefier --name "Netflix" "https://www.netflix.com/browse" --icon /home/brett/Pictures/netflix-icon.png
+nativefier --name "Whatsapp" "https://web.whatsapp.com"
 
 cp /home/brett/i3wm/Personal/nativefier/* /home/brett/.local/share/applications/
 
