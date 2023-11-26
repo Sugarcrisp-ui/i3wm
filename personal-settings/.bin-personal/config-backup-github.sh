bfea8d9335e698c69f6c4609f56654b06fb370dd
Brett Crisp <brettcrisp2@gmail.com>
2023-11-26 23:21:55 +0700

    added archlinux-betterlockscreen/settings.conf and arcolinux-welcome-app/settings.conf and systemd/user/settings and mimeapps.list and micro/conf and nano/conf and paru/conf

--- personal-settings/.bin-personal/config-backup-github.sh
+++ personal-settings/.bin-personal/config-backup-github.sh
@@ -8,13 +8,15 @@ DEST="/home/brett/Github/i3wm/personal-settings"
 
 # Directories
 rsync $RSYNC_OPTS /home/brett/.bin-personal/ $DEST/.bin-personal 
-rsync $RSYNC_OPTS /home/brett/.config/arcolinux-welcome-app/ $DEST/arcolinux-welcome-app
-rsync $RSYNC_OPTS /home/brett/.config/archlinux-betterlockscreen/ $DEST/.config/
+rsync $RSYNC_OPTS /home/brett/.config/arcolinux-welcome-app/ $DEST/.config/arcolinux-welcome-app
+rsync $RSYNC_OPTS /home/brett/.config/archlinux-betterlockscreen/ $DEST/.config/archlinux-betterlockscreen
 rsync $RSYNC_OPTS /home/brett/.config/Cryptomator/ $DEST/.config/Cryptomator
 rsync $RSYNC_OPTS /home/brett/.config/dconf/ $DEST/.config/dconf
 rsync $RSYNC_OPTS /home/brett/.config/expressvpn/ $DEST/.config/expressvpn
+rsync $RSYNC_OPTS /home/brett/.config/paru/ $DEST/.config/paru
 rsync $RSYNC_OPTS /home/brett/.config/polybar/ $DEST/.config/polybar
 rsync $RSYNC_OPTS /home/brett/.config/rofi/ $DEST/.config/rofi
+rsync $RSYNC_OPTS /home/brett/.config/systemd/ $DEST/.config/systemd
 rsync $RSYNC_OPTS /home/brett/.config/Thunar/ $DEST/.config/Thunar
 rsync $RSYNC_OPTS /home/brett/.config/variety/Fetched/ $DEST/.config/variety/Fetched
 rsync $RSYNC_OPTS /home/brett/.config/xfce4/ $DEST/.config/xfce4
@@ -28,6 +30,8 @@ rsync $RSYNC_OPTS /home/brett/.bashrc-personal $DEST/
 rsync $RSYNC_OPTS /home/brett/.config/i3/config $DEST/.config/i3/
 rsync $RSYNC_OPTS /home/brett/.config/Code/User/settings.json $DEST/.config/Code/User/settings.json
 rsync $RSYNC_OPTS /home/brett/.config/micro/settings.json $DEST/.config/micro/settings.json
+rsync $RSYNC_OPTS /home/brett/.config/mimeapps.list $DEST/.config/mimeapps.list
+rsync $RSYNC_OPTS /home/brett/.config/nano/nanorc $DEST/.config/nano/
 rsync $RSYNC_OPTS /home/brett/.config/qBittorrent/qBittorrent.conf $DEST/.config/qBittorrent/
 rsync $RSYNC_OPTS /etc/vconsole.conf $DEST/etc/
 rsync $RSYNC_OPTS /etc/rc.local $DEST/etc/
