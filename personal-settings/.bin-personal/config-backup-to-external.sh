eb4579e6611f7f46a0de3bb1b54cf34c57c77a65
Brett Crisp <brettcrisp2@gmail.com>
2023-11-26 22:37:25 +0700

    Added qBittorrent.conf in .config/qBittorrent

--- personal-settings/.bin-personal/config-backup-to-external.sh
+++ personal-settings/.bin-personal/config-backup-to-external.sh
@@ -3,20 +3,20 @@
 # This is a backup of my desktop to my external drive
 
 # Define common rsync options
-RSYNC_OPTS="-avz --delete --dirs --perms"
+RSYNC_OPTS="-avz -r --exclude=.cache --delete --perms"
 DEST="/run/media/brett/backup"
 
 # Directories
 
 rsync $RSYNC_OPTS /home/brett/.bin-personal/ $DEST/.bin-personal
 
-rsync $RSYNC_OPTS /home/brett/.local/share/Cryptomator/mnt/docs/ $DEST/cryptomator-data
+rsync $RSYNC_OPTS /home/brett/.local/share/Cryptomator/mnt/my-vault/ $DEST/cryptomator-data
 
-rsync $RSYNC_OPTS /home/brett/Documents/cryptomator/docs/ $DEST/cryptomator-config/docs
-
-rsync $RSYNC_OPTS --exclude={Insync,autostart,google-chrome*} /home/brett/.config/ $DEST/.config
+rsync $RSYNC_OPTS --exclude={Insync,google-chrome*} /home/brett/.config/ $DEST/.config
 
 rsync $RSYNC_OPTS /home/brett/.local/share/applications/ $DEST/.local/share/applications
+
+rsync $RSYNC_OPTS /home/brett/.vscode/ $DEST/.vscode
 
 #rsync $RSYNC_OPTS /home/brett/.local/share/ice/ $DEST/.local/share/ice
 
@@ -39,3 +39,7 @@ rsync $RSYNC_OPTS /usr/share/sddm/themes/arcolinux-sugar-candy/ $DEST/usr/share/
 rsync $RSYNC_OPTS /home/brett/.bashrc-personal $DEST/.bashrc-personal
 
 rsync $RSYNC_OPTS /home/brett/.config/variety/variety.conf $DEST/.config/variety/variety.conf
+rsync $RSYNC_OPTS /etc/vconsole.conf $DEST/etc/
+rsync $RSYNC_OPTS /etc/rc.local $DEST/etc/
+rsync $RSYNC_OPTS /etc/mkinitcpio.conf $DEST/etc/
+rsync $RSYNC_OPTS /usr/share/gvfs/mounts/network.mount $DEST/usr/share/gvfs/mounts/
