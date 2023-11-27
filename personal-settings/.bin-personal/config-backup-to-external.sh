09dcc78e2efffd85f7005dfd37afc05e84343686
Brett Crisp <brettcrisp2@gmail.com>
2023-11-27 16:22:12 +0700

    Organizing a couple of my backup scripts

--- personal-settings/.bin-personal/config-backup-to-external.sh
+++ personal-settings/.bin-personal/config-backup-to-external.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-# This is a backup of my desktop to my external drive
-
 # Define common rsync options
 RSYNC_OPTS="-avz -r --exclude=.cache --delete --perms"
 DEST="/run/media/brett/backup"
 
-# Directories
-
+# .bin-personal directory
 rsync $RSYNC_OPTS /home/brett/.bin-personal/ $DEST/.bin-personal
 
-rsync $RSYNC_OPTS /home/brett/.local/share/Cryptomator/mnt/my-vault/ $DEST/cryptomator-data
+# .config directories
+rsync $RSYNC_OPTS /home/brett/.config/arcolinux-welcome-app/ $DEST/.config/arcolinux-welcome-app
+rsync $RSYNC_OPTS /home/brett/.config/archlinux-betterlockscreen/ $DEST/.config/archlinux-betterlockscreen 
+rsync $RSYNC_OPTS /home/brett/.config/Cryptomator/ $DEST/.config/Cryptomator
+rsync $RSYNC_OPTS /home/brett/.config/dconf/ $DEST/.config/dconf
+rsync $RSYNC_OPTS /home/brett/.config/expressvpn/ $DEST/.config/expressvpn
+rsync $RSYNC_OPTS /home/brett/.config/paru/ $DEST/.config/paru
+rsync $RSYNC_OPTS /home/brett/.config/polybar/ $DEST/.config/polybar
+rsync $RSYNC_OPTS /home/brett/.config/rofi/ $DEST/.config/rofi
+rsync $RSYNC_OPTS /home/brett/.config/systemd/ $DEST/.config/systemd 
+rsync $RSYNC_OPTS /home/brett/.config/Thunar/ $DEST/.config/Thunar
+rsync $RSYNC_OPTS /home/brett/.config/variety/Fetched/ $DEST/.config/variety/Fetched
+rsync $RSYNC_OPTS /home/brett/.config/xfce4/ $DEST/.config/xfce4
 
-rsync $RSYNC_OPTS --exclude={Insync,google-chrome*} /home/brett/.config/ $DEST/.config
+# .config files
+rsync $RSYNC_OPTS /home/brett/.config/i3/config $DEST/.config/i3/
+rsync $RSYNC_OPTS /home/brett/.config/Code/User/settings.json $DEST/.config/Code/User/settings.json
+rsync $RSYNC_OPTS /home/brett/.config/micro/settings.json $DEST/.config/micro/settings.json  
+rsync $RSYNC_OPTS /home/brett/.config/mimeapps.list $DEST/.config/mimeapps.list
+rsync $RSYNC_OPTS /home/brett/.config/nano/nanorc $DEST/.config/nano/
+rsync $RSYNC_OPTS /home/brett/.config/qBittorrent/qBittorrent.conf $DEST/.config/qBittorrent/
 
-rsync $RSYNC_OPTS /home/brett/.local/share/applications/ $DEST/.local/share/applications
+# .vscode directory
+rsync $RSYNC_OPTS /home/brett/.vscode/ $DEST/.vscode  
 
-rsync $RSYNC_OPTS /home/brett/.vscode/ $DEST/.vscode
-
-#rsync $RSYNC_OPTS /home/brett/.local/share/ice/ $DEST/.local/share/ice
-
+# .ssh directory
 rsync $RSYNC_OPTS /home/brett/.ssh/ $DEST/.ssh
 
-#rsync $RSYNC_OPTS /home/brett/.var/ $DEST/.var
+# etc directories
+rsync $RSYNC_OPTS /etc/sddm.conf.d/ $DEST/etc/sddm.conf.d  
 
-rsync $RSYNC_OPTS /home/brett/.vnc/ $DEST/.vnc
-
-rsync $RSYNC_OPTS /home/brett/Downloads/ $DEST/Downloads
-
-rsync $RSYNC_OPTS /home/brett/Pictures/ $DEST/Pictures
-
-rsync $RSYNC_OPTS /usr/share/sddm/themes/arcolinux-sugar-candy/ $DEST/usr/share/sddm/themes/arcolinux-sugar-candy
-
-
-
-# files
-
-rsync $RSYNC_OPTS /home/brett/.bashrc-personal $DEST/.bashrc-personal
-
-rsync $RSYNC_OPTS /home/brett/.config/variety/variety.conf $DEST/.config/variety/variety.conf
+# etc files
 rsync $RSYNC_OPTS /etc/vconsole.conf $DEST/etc/
 rsync $RSYNC_OPTS /etc/rc.local $DEST/etc/
 rsync $RSYNC_OPTS /etc/mkinitcpio.conf $DEST/etc/
-rsync $RSYNC_OPTS /usr/share/gvfs/mounts/network.mount $DEST/usr/share/gvfs/mounts/
+
+# sddm themes directory 
+rsync $RSYNC_OPTS /usr/share/sddm/themes/arcolinux-sugar-candy/ $DEST/usr/share/sddm/themes/arcolinux-sugar-candy
--- personal-settings/.bin-personal/config-backup-to-personal.sh
+++ personal-settings/.bin-personal/config-backup-to-personal.sh
@@ -4,34 +4,22 @@
 RSYNC_OPTS="-avz --delete"
 DESTINATION="/personal/"
 
-# Directories
+# .bin-personal directory
+rsync $RSYNC_OPTS /home/brett/.bin-personal/ $DESTINATION/.bin-personal
 
-rsync $RSYNC_OPTS /home/brett/.bin-personal/ /$DESTINATION/.bin-personal
+# .config directories
+rsync $RSYNC_OPTS /home/brett/.config/arcolinux-welcome-app/ $DESTINATION/.config/arcolinux-welcome-app
+rsync $RSYNC_OPTS /home/brett/.config/archlinux-betterlockscreen/ $DESTINATION/.config/archlinux-betterlockscreen
+rsync $RSYNC_OPTS /home/brett/.config/dconf/ $DESTINATION/.config/dconf  
+rsync $RSYNC_OPTS /home/brett/.config/i3/ $DESTINATION/.config/i3
+rsync $RSYNC_OPTS /home/brett/.config/polybar/ $DESTINATION/.config/polybar
+rsync $RSYNC_OPTS /home/brett/.config/Thunar/ $DESTINATION/.config/Thunar 
+rsync $RSYNC_OPTS /home/brett/.config/variety/Fetched/ $DESTINATION/.config/variety/Fetched
+rsync $RSYNC_OPTS /home/brett/.config/xfce4/ $DESTINATION/.config/xfce4
 
-rsync $RSYNC_OPTS /home/brett/.config/arcolinux-welcome-app/ /$DESTINATION/.config/arcolinux-welcome-app
+# .local directories
+rsync $RSYNC_OPTS /home/brett/.local/share/applications/ $DESTINATION/.local/share/applications
 
-#rsync $RSYNC_OPTS /home/brett/.config/autostart/ /$DESTINATION/.config/autostart
-
-rsync $RSYNC_OPTS /home/brett/.config/arcolinux-betterlockscreen/ /$DESTINATION/.config/arcolinux-betterlockscreen
-
-rsync $RSYNC_OPTS /home/brett/.config/dconf/ /$DESTINATION/.config/dconf
-
-rsync $RSYNC_OPTS /home/brett/.config/i3/ /$DESTINATION/.config/i3
-
-rsync $RSYNC_OPTS /home/brett/.config/polybar/ /$DESTINATION/.config/polybar
-
-rsync $RSYNC_OPTS /home/brett/.config/Thunar/ /$DESTINATION/.config/Thunar
-
-rsync $RSYNC_OPTS /home/brett/.config/variety/Fetched/ /$DESTINATION/.config/variety/Fetched
-
-rsync $RSYNC_OPTS /home/brett/.config/xfce4/ /$DESTINATION/.config/xfce4
-
-rsync $RSYNC_OPTS /home/brett/.local/share/applications/ /$DESTINATION/.local/share/applications
-
-
-# files
-
-rsync $RSYNC_OPTS /home/brett/.config/variety/variety.conf /$DESTINATION/.config/variety/variety.conf
-
-rsync $RSYNC_OPTS /home/brett/.config/mimeapps.list /$DESTINATION/.config/mimeapps.list
-
+# .config files  
+rsync $RSYNC_OPTS /home/brett/.config/variety/variety.conf $DESTINATION/.config/variety/variety.conf  
+rsync $RSYNC_OPTS /home/brett/.config/mimeapps.list $DESTINATION/.config/mimeapps.list
