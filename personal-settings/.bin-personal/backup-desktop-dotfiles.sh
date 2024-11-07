e680fae7410022d9b45b8117646152fd84069496
Brett Crisp <brettcrisp2@gmail.com>
2024-11-07 10:45:08 +0700

    Added new script files to .bin-personal

---
+++ personal-settings/.bin-personal/backup-desktop-dotfiles.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Define source and destination directories
+SOURCE_DIR="$HOME/dotfiles/"
+DEST_DIR="/media/brett/backup/dotfiles-desktop/"
+LOG_FILE="$HOME/backup.log"
+
+# Use rsync to copy files with .gitignore-style exclusions
+rsync -av --delete \
+     --include 'bash/***' \
+    --include 'config/.config/' \
+    --include 'config/.config/bluetooth/***' \
+    --include 'config/.config/i3/***' \
+    --include 'config/.config/i3-auto-layout/***' \
+    --include 'config/.config/nemo/***' \
+    --include 'config/.config/picom/***' \
+    --include 'config/.config/polybar/***' \
+    --include 'config/.config/Proton/***' \
+    --include 'config/.config/qBittorrent/***' \
+    --include 'config/.config/rofi/***' \
+    --include 'config/.config/i3-xdg-terminals.list' \
+    --include 'config/.config/input.conf' \
+    --include 'config/.config/main.conf' \
+    --include 'config/.config/mimeapps.list' \
+    --include 'config/.config/network.conf' \
+    --include 'config/.config/pavucontrol.ini' \
+    --include 'config/.config/QtProject.conf' \
+    --include 'config/.config/user-dirs.dirs' \
+    --include 'config/.config/user-dirs.locale' \
+    --include 'config/.config/xdg-terminals.list' \
+    --include 'feh/***' \
+    --include 'fonts/***' \
+    --include 'i3status/***' \
+    --include 'ssh/***' \
+    --include 'git-v1.sh' \
+    --include 'git-v2.sh' \
+    --include 'setup-git-v1.sh' \
+    --include 'setup-git-v2.sh' \
+    --include '*.swp' \
+    --exclude '*' \
+    "$SOURCE_DIR" "$DEST_DIR"
