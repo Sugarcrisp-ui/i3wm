8b5154343d4afafb3bb00c121a327427bb69414d
Brett Crisp <brettcrisp2@gmail.com>
2024-12-03 12:03:26 +0700

    Update 110-install-core-software.sh

--- Personal/110-install-core-software.sh
+++ Personal/110-install-core-software.sh
@@ -2,91 +2,93 @@
 
 # Author: Brett Crisp
 
-# This script installs a list of packages on an Arch Linux system.
+# Color definitions
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
 
-# Function to handle errors
-handle_error() {
-  echo -e "\e[31mAn error occurred while installing the packages. Please check the output and try again.\e[0m"
-  exit 1
-}
-
-# Set up error handling
-trap 'handle_error' ERR
-
-# Function to check if a package is installed.
 function is_installed() {
-  pacman -Qi "$1" &> /dev/null
+    pacman -Qi "$1" &> /dev/null
 }
 
-# Function to install a package.
 function install_package() {
-  if ! is_installed "$1"; then
-    echo -e "\e[32mInstalling package $1\e[0m"
-    sudo pacman -S --noconfirm --needed "$1"
-  fi
+    if ! is_installed "$1"; then
+        echo "${CYAN}Installing: $1${RESET}"
+        sudo pacman -S --noconfirm --needed "$1"
+    else
+        echo "${GREEN}Already installed: $1${RESET}"
+    fi
 }
 
-# Function to install a category of packages.
-function install_category() {
-  echo -e "\e[32mInstalling software for category $1\e[0m"
-  for package in "${list[$1]}"; do
-    install_package "$package"
-  done
-}
+# Print header
+echo "${BLUE}################################################################"
+echo "                Installing Core Software Packages"
+echo "################################################################${RESET}"
 
-# List of packages to install.
-list=(
-# arcolinux-teamviewer
-# arcolinux-sddm-sugar-candy-git
-bitwarden
-blueman
-baobab
-brave-bin
-# grub-btrfs
-copyq
-cronie
-ffmpeg
-flatpak
-geany
-#github-desktop-bin
-gnome-calculator
-gpick
-gufw
-insync
-libreoffice-still
-meld
-micro
-pamac-aur
-paprefs
-pinta
-polybar
-potrace
-powerline
-qbittorrent
-qt5-graphicaleffects
-qt5-svg
-qt6ct
-realvnc-vnc-server
-realvnc-vnc-viewer
-rofi
-seahorse
-# simplescreenrecorder
- spotify
-sshfs
-stow
-timeshift
-# Used with btrfs   timeshift-autosnap
-virtualbox
-visual-studio-code-bin
-webapp-manager
-xclip
-zip
+# Core software packages
+packages=(
+    # arcolinux-teamviewer
+    # arcolinux-sddm-sugar-candy-git
+    bitwarden
+    blueman
+    baobab
+    brave-bin
+    # grub-btrfs
+    copyq
+    cronie
+    fd
+    ffmpeg
+    flatpak
+    geany
+    #github-desktop-bin
+    gnome-calculator
+    gpick
+    gufw
+    insync
+    libreoffice-still
+    meld
+    micro
+    pamac-aur
+    paprefs
+    pinta
+    polybar
+    potrace
+    powerline
+    qbittorrent
+    qt5-graphicaleffects
+    qt5-svg
+    qt6ct
+    realvnc-vnc-server
+    realvnc-vnc-viewer
+    ripgrep
+    rofi
+    seahorse
+    # simplescreenrecorder
+    spotify
+    sshfs
+    stow
+    timeshift
+    # Used with btrfs timeshift-autosnap
+    virtualbox
+    visual-studio-code-bin
+    webapp-manager
+    xclip
+    zip
 )
 
-# Install all of the packages.
-for category in "${!list[@]}"; do
-  install_category "$category"
+# Install packages
+echo "${CYAN}Installing core software...${RESET}"
+total=${#packages[@]}
+current=0
+
+for package in "${packages[@]}"; do
+    [[ $package == \#* ]] && continue
+    ((current++))
+    echo "${BLUE}[${current}/${total}] Processing package: ${package}${RESET}"
+    install_package "$package"
 done
 
-# Success message.
-echo -e "\e[32mSoftware has been installed\e[0m"
+echo "${GREEN}################################################################"
+echo "                Core Software Installation Complete!"
+echo "################################################################${RESET}"
