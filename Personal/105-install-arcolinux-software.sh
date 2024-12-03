18ba9d213c8928aa3870960dce0383242046832b
Brett Crisp <brettcrisp2@gmail.com>
2024-12-03 12:03:02 +0700

    Update 105-install-arcolinux-software.sh

--- Personal/105-install-arcolinux-software.sh
+++ Personal/105-install-arcolinux-software.sh
@@ -1,58 +1,52 @@
 #!/bin/bash
 
-# The set command is used to determine action if error
-# is encountered.  (-e) will stop and exit (+e) will
-# continue with the script.
-set -e
-trap 'handle_error' ERR
+# Color definitions
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
 
-# Function to handle errors
-handle_error() {
-  echo -e "\e[31mAn error occurred while installing the ArcoLinux software. Please check the output and try again.\e[0m"
-  exit 1
-}
-
-# Function to check if ArcoLinux repos are in /etc/pacman.conf
+# Check for ArcoLinux repos
 function check_repos() {
-  if grep -q arcolinux_repo /etc/pacman.conf; then
-    echo
-    tput setaf 2
-    echo "################################################################"
-    echo "################ ArcoLinux repos are already in /etc/pacman.conf"
-    echo "################################################################"
-    tput sgr0
-    echo
-  else
-    echo
-    tput setaf 2
-    echo "################################################################"
-    echo "################ Getting the keys and mirrors for ArchLinux"
-    echo "################################################################"
-    tput sgr0
-    echo
-    sh arch/get-the-keys-and-repos.sh
-    sudo pacman -Sy
-  fi
+    echo "${CYAN}Checking ArcoLinux repositories...${RESET}"
+    if grep -q arcolinux_repo /etc/pacman.conf; then
+        echo "${GREEN}ArcoLinux repos already present${RESET}"
+    else
+        echo "${CYAN}Getting ArchLinux keys and mirrors${RESET}"
+        sh arch/get-the-keys-and-repos.sh
+        sudo pacman -Sy
+    fi
 }
 
-# Function to install ArcoLinux/ArchLinux software
+# Install ArcoLinux/ArchLinux software
 function install_software() {
-  sudo pacman -S --noconfirm --needed appstream
-  sudo pacman -S --noconfirm --needed archlinux-logout-git
-  sudo pacman -S --noconfirm --needed archlinux-tweak-tool-git
-  sudo pacman -S --noconfirm --needed arcolinux-bin-git
-  sudo pacman -S --noconfirm --needed arcolinux-hblock-git
-  sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git
+    echo "${BLUE}################################################################"
+    echo "                Installing ArcoLinux Software"
+    echo "################################################################${RESET}"
 
-  echo
-  tput setaf 2
-  echo "################################################################"
-  echo "################ ArcoLinux software installed"
-  echo "################################################################"
-  tput sgr0
-  echo
+    packages=(
+        "appstream"
+        "archlinux-logout-git"
+        "archlinux-tweak-tool-git"
+        "arcolinux-bin-git"
+        "arcolinux-hblock-git"
+        "arcolinux-wallpapers-git"
+    )
+
+    total=${#packages[@]}
+    current=0
+
+    for package in "${packages[@]}"; do
+        ((current++))
+        echo "${CYAN}[${current}/${total}] Installing: ${package}${RESET}"
+        sudo pacman -S --noconfirm --needed "$package"
+    done
+
+    echo "${GREEN}################################################################"
+    echo "            ArcoLinux Software Installation Complete!"
+    echo "################################################################${RESET}"
 }
 
-# Check for ArcoLinux repos and install the software
+# Run installation
 check_repos
 install_software
