53ee18e3f73392e94e187692b6d2f0952c766cac
Brett Crisp <brettcrisp2@gmail.com>
2024-12-03 12:06:17 +0700

    Update 666-remove-software.sh

--- Personal/666-remove-software.sh
+++ Personal/666-remove-software.sh
@@ -1,64 +1,52 @@
 #!/bin/bash
 
-# The set command is used to determine action if error
-# is encountered.  (-e) will stop and exit (+e) will
-# continue with the script.
-set -e
-trap 'handle_error' ERR
+# Color definitions
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
+YELLOW=$(tput setaf 3)
+RESET=$(tput sgr0)
 
-# Function to handle errors
-handle_error() {
-  echo -e "\e[31mAn error occurred while uninstalling the packages. Please check the output and try again.\e[0m"
-  exit 1
+function check_for_nvidia() {
+    lspci | grep -i nvidia &> /dev/null
 }
 
-# Function to check if an NVIDIA GPU is present
-check_for_nvidia() {
-  if lspci | grep -i nvidia &> /dev/null; then
-    return 0 # NVIDIA GPU found
-  else
-    return 1 # No NVIDIA GPU found
-  fi
+function remove_package() {
+    if pacman -Qi "$1" &> /dev/null; then
+        echo "${CYAN}Removing: $1${RESET}"
+        sudo pacman -Rs "$1" --noconfirm
+    else
+        echo "${YELLOW}Not installed: $1${RESET}"
+    fi
 }
 
-# Function to remove a package
-function_remove() {
-  if pacman -Qi "$1" &> /dev/null; then
-    tput setaf 1
-    echo "################################################################"
-    echo "######    "$1" is installed and will be removed now."
-    echo "################################################################"
-    echo
-    tput sgr0
-    sudo pacman -Rs "$1" --noconfirm
-  else
-    tput setaf 2
-    echo "################################################################"
-    echo "######    "$1" was not present or already removed."
-    echo "################################################################"
-    echo
-    tput sgr0
-  fi
-}
+echo "${BLUE}################################################################"
+echo "                    Removing Unnecessary Software"
+echo "################################################################${RESET}"
 
-# List of packages to uninstall
+# Packages to remove
 packages_to_uninstall=(
-  blueberry
+    blueberry
 )
 
-# Check for NVIDIA GPU and add xf86-video-nouveau to the uninstall list if not found
+# Add nouveau if no NVIDIA GPU
 if ! check_for_nvidia; then
-  packages_to_uninstall+=("xf86-video-nouveau")
-  echo -e "\e[32mNo NVIDIA GPU detected. Adding xf86-video-nouveau to uninstall list.\e[0m"
+    packages_to_uninstall+=("xf86-video-nouveau")
+    echo "${CYAN}No NVIDIA GPU - will remove nouveau driver${RESET}"
 else
-  echo -e "\e[33mNVIDIA GPU detected. Skipping xf86-video-nouveau uninstallation.\e[0m"
+    echo "${YELLOW}NVIDIA GPU detected - keeping nouveau driver${RESET}"
 fi
 
-# Uninstall packages
+# Remove packages
+total=${#packages_to_uninstall[@]}
+current=0
+
 for package in "${packages_to_uninstall[@]}"; do
-  function_remove "$package"
+    ((current++))
+    echo "${BLUE}[${current}/${total}] Processing package: ${package}${RESET}"
+    remove_package "$package"
 done
 
-echo -e "\e[32m################################################################"
-echo "######                    packages uninstalled            ######"
-echo "################################################################\e[0m"
+echo "${GREEN}################################################################"
+echo "                    Software Removal Complete!"
+echo "################################################################${RESET}"
