f9239dcd36793ea0025d679c7f15643b2c32d412
Brett Crisp <brettcrisp2@gmail.com>
2024-12-03 12:05:57 +0700

    Update 270-enable-hblock.sh

--- Personal/270-enable-hblock.sh
+++ Personal/270-enable-hblock.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-# Set up error handling
-set -e
-trap 'echo "An error occurred at line $LINENO. Exiting." >&2; exit 1' ERR
+# Color definitions
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
 
-# Function for colored logging
-log_with_color() {
-    tput setaf 11; echo "################################################################"; tput sgr0
-    tput setaf 11; echo "$1"; tput sgr0
-    tput setaf 11; echo "################################################################"; tput sgr0
+function log_message() {
+    echo "${BLUE}################################################################"
+    echo "$1"
+    echo "################################################################${RESET}"
 }
 
-# This script will automate the setup of hblock on an Arch Linux system
+# Update hosts file
+log_message "Starting Host File Update"
+echo "${CYAN}Running hblock to update system hosts file...${RESET}"
 
-log_with_color "Running hblock to update /etc/hosts"
+sudo hblock && echo "${GREEN}Host file update completed successfully!${RESET}"
 
-# Run hblock with default settings to update /etc/hosts
-sudo hblock
-
-# Check if hblock ran successfully
-if [ $? -eq 0 ]; then
-    log_with_color "hblock has successfully updated the hosts file."
-else
-    log_with_color "There was an error running hblock. Please check the command output for details."
-    exit 1
-fi
+log_message "Host File Protection Enabled"
